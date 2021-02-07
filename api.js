04eda0aaf0b5a380427a4e186ecb712a8f90d8d2
Meherin Akter <meherinakter13@gmail.com>
2021-02-07 21:27:21 +0600

    color change

--- api.js
+++ api.js
@@ -33,16 +33,19 @@ const displayIngredient = meal=>{
     const ingredientContainer= document.getElementById("ingredientContainer");
     ingredientContainer.innerHTML =`
     <img id="image" src="${meal.strMealThumb}">
+    <div id ="ingredientsValue">
     <h1> ${meal.strMeal}</h1>
-    <h3>ingredients</h3>
+    <h3>Ingredients</h3>
     <li>${meal.strMeasure1}${meal.strIngredient1}</li>
     <li>${meal.strMeasure2}${meal.strIngredient2}</li>
     <li>${meal.strMeasure3}${meal.strIngredient3}</li>
     <li>${meal.strMeasure4}${meal.strIngredient4}</li>
     <li>${meal.strMeasure5}${meal.strIngredient5}</li>
-    <li>${meal.strMeasure7}${meal.strIngredient6}</li>
-    <li>${meal.strMeasure6}${meal.strIngredient7}</li>
-    <li>${meal.strMeasure7}${meal.strIngredient8}</li>
+    <li>${meal.strMeasure6}${meal.strIngredient6}</li>
+    <li>${meal.strMeasure7}${meal.strIngredient7}</li>
+    <li>${meal.strMeasure8}${meal.strIngredient8}</li>
+    <li>${meal.strMeasure9}${meal.strIngredient9}</li>
+    </div>
     `;
     
 }
